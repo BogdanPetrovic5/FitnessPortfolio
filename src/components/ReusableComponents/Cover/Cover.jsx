4c2714f4974bb8a5ea39ba47fc30baba1358c77c
Bogdan <94244233+BogdanPetrovic5@users.noreply.github.com>
2025-04-25 14:43:24 +0200

    Cover update, seo optimization

--- src/components/ReusableComponents/Cover/Cover.jsx
+++ src/components/ReusableComponents/Cover/Cover.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import './Cover.css'
 
 import Ruka from './images/ruka.jpeg'
@@ -10,132 +10,93 @@ import TT from './images/tik-tok.png'
 import FB from './images/facebook.png'
 import { Link, useLocation  } from 'react-router-dom';
 function Cover(){
-   return(
-    <section className='Cover'  style={{backgroundImage: `url('https://assets-global.website-files.com/65656d367ef06b6df2247915/657029cc9670bafa97954427_muscular-sportsman-building-biceps-with-dumbbell_73891bba2b5546b1bb17eef7d6d184ea%201.webp')`}}>
-      
-         <div className='Title' data-aos="fade-right" data-aos-duration="1500">
-              <div className='Cover-Title'>
-                     <div className='Title-Decoration' 
-                            data-aos-duration="1000">
-                            <div></div> 
-                            <h1>WITH DJOLE THROUGH HELL</h1>
-                     </div>
-                     <h1 className='Cover-Title-h1'>
-                     Lorem Ipsum is simply <span>dummy text</span> 
-                     </h1>
-                     <Link to="/about"> 
-                            <button>LEARN MORE</button>
-                     </Link>
-              </div>
-              
-              <div className='Overview'>
-                     <div className='Overview-Wrap'>
-                            <div className='Clients Overview-Item'>
-                                   <h1>
-                                       130+   
-                                   </h1>
-                                   <p>CLients</p>
+       const [clients, setClients] = useState(0);
+       const [patients, setPatients] = useState(0);
+       const [experience, setExperience] = useState(0);
+
+       const clientsRef = useRef(null);
+       const patientsRef = useRef(null);
+       const experienceRef = useRef(null);
+
+       const animateNumber = (ref, setState, target, time) =>{
+              const observer = new IntersectionObserver((entries, observer)=>{
+                     entries.forEach(entry => {
+                            if(entry.isIntersecting){
+                                   let count = 0;
+                                   const interval = setInterval(()=>{
+                                          count++;
+                                          setState(count)
+                                          if(count == target) clearInterval(interval);
+                                   },time)
+                                   observer.unobserve(entry.target)
+                            }
+                     })
+              }, {threshold: 0.5})
+              observer.observe(ref.current);
+       }
+       useEffect(() => {
+              animateNumber(clientsRef, setClients, 130, 20);
+              animateNumber(patientsRef, setPatients, 1000, 2);
+              animateNumber(experienceRef, setExperience, 4, 300);
+       }, []);
+       return(
+              <header className='Cover' 
+              style={{backgroundImage: `url('https://assets-global.website-files.com/65656d367ef06b6df2247915/657029cc9670bafa97954427_muscular-sportsman-building-biceps-with-dumbbell_73891bba2b5546b1bb17eef7d6d184ea%201.webp')`}}
+              aria-label="Cover section showcasing personal trainer"
+              >
+                     <div className='Title' data-aos="fade-right" data-aos-duration="1500">
+                            <div className='Cover-Title'>
+                                   <div className='Title-Decoration' 
+                                          data-aos-duration="1000">
+                                          <div></div> 
+                                          <h1>WITH DJOLE THROUGH HELL</h1>
+                                   </div>
+                                   <h2 className='Cover-Title-h1'>
+                                   Lorem Ipsum is simply <span>dummy text</span> 
+                                   </h2>
+                                   <Link to="/about" aria-label="Learn more about Personal Trainer's journey"> 
+                                          <button>LEARN MORE</button>
+                                   </Link>
                             </div>
-                            <div className='Patients Overview-Item'>
-                                   <h1>
-                                       1000+ 
-                                   </h1>
-                                   <p>Patients</p>
-                            </div>
-                            <div className='Experience Overview-Item'>
-                                   <h1>
-                                       4+ 
-                                   </h1>
-                                   <p>Years of experience</p>
-                            </div>
-                     </div>
-                     <div className='Overview-Socials'>
-                            <div className='Overview-Socials-TikTok Soc'>
-                                   <img src={TT}>
-                                   </img>
-                            </div>
-                            <div className='Overview-Socials-Facebook Soc'>
-                            <img src={IG}>
-                                   </img>
-                            </div >
-                            <div className='Overview-Socials-Instagram Soc'>
-                            <img src={FB}>
-                                   </img>
-                            </div>
-                     </div>
-              </div>
-       </div>
-       <div className='Cover-Images'>
-              <div className='Cover-Images-Content'>
-                     {/* 
-                     <div className='Second-Bar' style={{ backgroundImage: `url(${Sise})` }} data-aos="fade-up"   data-aos-delay="500">
-                           
-                     </div>
-                     <div className='Third-Bar' data-aos="fade-up"  data-aos-delay="1500">
-
-                     </div>
-                     <div className='Fourth-Bar' style={{ backgroundImage: `url(${Podlaktice})` }} data-aos="fade-down"  data-aos-delay="800">
-
-                     </div>
-                     <div className='Fifth-Bar' data-aos="fade-up"  data-aos-delay="1600">
-
-                     </div>
-                     <div className='Sixth-Bar' style={{ backgroundImage: `url(${Ledja})` }} data-aos="fade-up"  data-aos-delay="1100">
-
-                     </div>
-                     <div className='Seventh-Bar'data-aos="fade-up"  data-aos-delay="1900">
-
-                     </div>
-                     <div className='Eight-Bar'data-aos="fade-up"  data-aos-delay="1900">
-
-                     </div> */}
-
-                     {/* <div className='First-Bar' data-aos="fade-up" data-aos-delay="1300">
-
-                     </div> */}
-                     <div className='First-Image' data-aos="slide-down" data-aos-duration = "900">
-                            <div className='Second-Bar' style={{ backgroundImage: `url(${Ruka})` }}data-aos-delay="200">
-                                   
-                            </div>
-                            <div className='First-Bar' data-aos-delay="500">
-
-                            </div>
-                            <div className='Third-Bar' data-aos-delay="800">
-
+                            
+                            <div className='Overview'>
+                                   <div className='Overview-Wrap'>
+                                          <div className='Clients Overview-Item'>
+                                                 <h1 ref={clientsRef}>
+                                                        {clients}+   
+                                                 </h1>
+                                                 <p>Clients</p>
+                                          </div>
+                                          <div className='Patients Overview-Item'>
+                                                 <h1 ref={patientsRef}>
+                                                        {patients}+ 
+                                                 </h1>
+                                                 <p>Patients</p>
+                                          </div>
+                                          <div className='Experience Overview-Item'>
+                                                 <h1 ref={experienceRef}>
+                                                        {experience}+ 
+                                                 </h1>
+                                                 <p>Years of experience</p>
+                                          </div>
+                                   </div>
+                                   <div className='Overview-Socials'>
+                                          <div className='Overview-Socials-TikTok Soc'>
+                                                 <img src={TT} alt='TikTok social media icon for personal trainer'>
+                                                 </img>
+                                          </div>
+                                          <div className='Overview-Socials-Facebook Soc'>
+                                          <img src={IG} alt='Instagram social media icon for personal trainer'>
+                                                 </img>
+                                          </div >
+                                          <div className='Overview-Socials-Instagram Soc'>
+                                          <img src={FB} alt='Facebook social media icon for personal trainer'>
+                                                 </img>
+                                          </div>
+                                   </div>
                             </div>
                      </div>
-                     
-                     {/*  */}
-                     <div className='Second-Image'  data-aos="slide-up" data-aos-duration = "900">
-                            <div className='Fourth-Bar' style={{ backgroundImage: `url(${Podlaktice})` }}   data-aos-delay="1100">
-
-                            </div>
-                            <div className='Fifth-Bar' data-aos-delay="1400" >
-
-                            </div>
-                     </div>
-                     
-                     {/*  */}
-                     <div className='Third-Image' data-aos="slide-down" data-aos-duration = "900">
-                            <div className='Sixth-Bar' style={{ backgroundImage: `url(${Ledja})` }} data-aos-delay="1700">
-
-                            </div>
-                            <div className='Seventh-Bar' data-aos-delay="2000">
-
-                            </div>
-                            <div className='Eight-Bar'data-aos-delay="2300">
-
-                            </div>
-                     </div>
-                     
-                     {/* <div className='Seventh-Bar'data-aos="fade-up"  data-aos-delay="1900">
-
-                     </div>
-                      */}
-              </div>
-       </div>
-       
-    </section>
-   );
+              </header>
+       );
 }
 export default Cover;
