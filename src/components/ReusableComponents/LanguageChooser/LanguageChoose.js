339fac13ce6a18afdaf6af02c91eba94d385350f
Bogdan <94244233+BogdanPetrovic5@users.noreply.github.com>
2025-05-12 14:15:46 +0200

    language picker anim update

--- src/components/ReusableComponents/LanguageChooser/LanguageChoose.js
+++ src/components/ReusableComponents/LanguageChooser/LanguageChoose.js
@@ -8,6 +8,8 @@ import { useTranslation } from 'react-i18next';
 function LanguageChooser(){
     const {t,i18n} = useTranslation();
     const languageButton = useRef(null);
+    const languageOptions = useRef(null);
+    const blurLayer = useRef(null);
     const languages = [
       { code: 'sr', label: 'Serbian', icon: '/assets/serbia.png' },
       { code: 'en', label: 'English', icon: '/assets/united-kingdom.png' }
@@ -19,15 +21,32 @@ function LanguageChooser(){
     const handleSelect = (lang) => {
       i18n.changeLanguage(lang.code);
       setSelectedLang(lang);
+      
       setIsOpen(false);
     };
+    const toggle = () =>{
+      if(isOpen == true){
+        languageOptions.current.classList.add("scale-down");
+       
+ 
+        setTimeout(()=>{
+          setIsOpen(false);
+          
+        },200)
+      }else{
+        languageOptions.current.classList.remove("scale-down");
+       
+    
+        setIsOpen(true);
+      }
+    }
     return(
-        <div  className={`language ${isOpen ? 'no-radius' : ''}`} ref={languageButton} onClick={() => setIsOpen(!isOpen)}>
+        <div  className={`language ${isOpen ? 'no-radius' : ''}`} ref={languageButton} onClick={() => toggle()}>
             <figure>
               <img src={selectedLang.icon} alt="" />
             </figure>
-            <div className='blur-layer'></div>
-            <div className={`language-options ${isOpen ? 'open' : ''}`}>
+            <div className={`blur-layer ${isOpen ? 'blur-layer-no-radius' : ''}`} ref={blurLayer}></div>
+            <div className={`language-options ${isOpen ? 'open' : ''}`} ref={languageOptions}>
               {languages.map((lang) => (
                 <figure key={lang.code} onClick={() => handleSelect(lang)}>
                   <img src={lang.icon} alt={lang.label} />
